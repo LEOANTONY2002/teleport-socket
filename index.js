d32b83582da715e6bc57f479026f9e192ae6948a
LEO ANTONY <leoantony20025@gmail.com>
2022-09-10 10:08:10 +0530

    socket

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 
+app.use(cors());
+app.use(express.json());
+
 const io = new Server(server, {
     cors: {
         origin: "https://leoteleport.herokuapp.com",
