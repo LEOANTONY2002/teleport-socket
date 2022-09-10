019ad9f0ac6bddaf4c732a27aa62cb781d4e7903
LEO ANTONY <leoantony20025@gmail.com>
2022-09-10 10:09:41 +0530

    socket

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
